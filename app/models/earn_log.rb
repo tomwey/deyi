bf4ff9ae34f8660ffefc503e51ef52cd87051720
Tomwey <tomwey@163.com>
2016-08-20 23:18:17 +0800

    fixed bug

--- app/models/earn_log.rb
+++ app/models/earn_log.rb
@@ -80,12 +80,12 @@ class EarnLog < ActiveRecord::Base
   end
 
   # json presentation
-  def title
-    case self.earnable_type
-    when 'Checkin' then '签到'
-    else ''
-    end
-  end
+  # def title
+  #   case self.earnable_type
+  #   when 'Checkin' then '签到'
+  #   else ''
+  #   end
+  # end
   
   def unit
     '益豆'
